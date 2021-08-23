078de9e1ef5454131390ed21810f23851d827eef
sk211 <sujanmajumder211@gmail.com>
2021-08-23 21:31:29 +0600

    recap all before milestone

---
+++ milestone-6/module-30/AIP and recap/basic.js
@@ -0,0 +1,53 @@
+// vairalbe 
+var deposit = 400;
+
+// condation 
+if (deposit > 500) {
+
+}
+if (deposit < 200) {
+
+}
+if (deposit == 500) {
+
+}
+if (deposit != 500) {
+
+}
+if (deposit >= 5500) {
+
+}
+if (deposit <= 500) {
+
+}
+// Array 
+const number = [1, 2, 3, 4, 5, 6, 8, 9]
+
+// ktoa gola ace 
+const numberCount = number.length;
+number.push(202020);
+number.pop()
+number[2] == 5555;
+// check the element 
+if (number.indexof(222) != -1) {
+    // if element it have the array the check 
+}
+if (number.includes(2222)) {
+    // element the che in array 
+}
+
+// loop 
+// while for 
+
+for (const number of numbers) {
+
+}
+// function 
+function functoinName(firest, last) {
+    const name = firest + last
+
+}
+functoinName("sujan" + "majumder")
+
+// object 
+const bottol
