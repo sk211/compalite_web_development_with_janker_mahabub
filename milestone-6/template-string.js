8a8531f7749b842e353bf4547d26391b89a08f59
sk211 <sujanmajumder211@gmail.com>
2021-08-24 23:42:48 +0600

    add template string

---
+++ milestone-6/template-string.js
@@ -0,0 +1,21 @@
+const priya = 'sujan majumder';
+const meye = "meye tumi ki dukkle cino cino na "
+const kobita = `kobita tum shopon carini`
+// not working 
+// const multiLine = "this is frirst line this 
+// this is second line
+// third line
+// "
+const multilNew = `this is 1st line 
+second lien and theidr line amar sonar bnagla ami tumai valobasi amr sonar nbnalg amit tumai valobasi arma sonar bangla `;
+const firendCount = 5;
+const oldHeader = `<h3 class="friend>Friend-5</h3>`
+const old2 = '<h3 class="friend>+Friend-' + firendCount + '</h3>'
+const old3 = `<h3 class="friend>friend-${firendCount}</h3>`
+console.log(old3);
+
+const first = "sujan"
+const last = "majumder"
+const old = 26
+const full = `amar first name ${first} amr second name ${last} amar boyes is ${old}`
+console.log(full);
