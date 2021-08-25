4480214f51a30050ed98db4b82292cfb209e1bfb
sk211 <sujanmajumder211@gmail.com>
2021-08-25 08:40:30 +0600

    mailstone 6 spreed paramater

---
+++ milestone-6/spreed-paramater.js
@@ -0,0 +1,11 @@
+const numbers = [1, 20, 54, 23, 24, 09]
+console.log(numbers);
+console.log(...numbers);
+
+const max = Math.min(10, 50, 120, 200)
+console.log(max);
+const maxInArray = Math.max(...numbers)
+console.log(maxInArray);
+
+const number2 = [22, "sujan", ...numbers, 88]
+console.log(number2);
